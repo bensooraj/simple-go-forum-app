cc7f7490d369dca3c6bd82e69304c79a362dd5c0
Ben Sooraj <bensoorajm@gmail.com>
2018-12-28 14:56:48 +0530

    first route

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -12,6 +13,20 @@ func main() {
 	fileServer := http.FileServer(fileDir)
 
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
+
+	mux.HandleFunc("/", index)
+	// mux.Handle("/err", err)
+
+	// mux.Handle("/login", login)
+	// mux.Handle("/logout", logout)
+	// mux.Handle("/signup", signup)
+	// mux.Handle("/signup_account", signup_account)
+	// mux.Handle("/authenticate", authenticate)
+
+	// mux.Handle("/thread/new", newThread)
+	// mux.Handle("/thread/create", createThread)
+	// mux.Handle("/thread/post", postThread)
+	// mux.Handle("/thread/read", readThread)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
@@ -37,4 +52,21 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	// files := []string{
+	// 	"template/layout.html",
+	// 	"template/navbar.html",
+	// 	"template/index.html",
+	// }
+
+	// templates := template.Must(template.ParseFiles(files...))
+
+	// threads, err := data.Threads()
+	// if err == nil {
+	// 	templates.ExecuteTemplate(w, "layout", threads)
+	// }
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, World!")
 }
