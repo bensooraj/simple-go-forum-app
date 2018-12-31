c707d48f465ffce1f614af66e6d4f060fe03e8e8
Ben Sooraj <bensoorajm@gmail.com>
2018-12-31 17:27:50 +0530

    playing with cookies

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
+
+// Post .
+type Post struct {
+	User    string
+	Threads []string
+}
 
 func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.WriteHeader(http.StatusOK)
@@ -83,7 +92,84 @@ func writeHeaderExample(w http.ResponseWriter, r *http.Request, p httprouter.Par
 
 func headerExample(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	w.Header().Set("Location", "http://google.com")
-	w.WriteHeader(http.StatusTemporaryRedirect)
+	w.WriteHeader(301)
+}
+
+func jsonExample(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+	post := Post{
+		User: "Ben Sooraj",
+		Threads: []string{
+			"Hello, World!",
+			"Test thread #1",
+			"Test thread #2",
+			"Test thread #3",
+		},
+	}
+	jsonData, _ := json.Marshal(&post)
+	w.Write(jsonData)
+}
+
+func setCookie(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	c1 := http.Cookie{
+		Name:     "first_cookie",
+		Value:    "Go Web Programming0",
+		HttpOnly: true,
+	}
+
+	c2 := http.Cookie{
+		Name:     "second_cookie",
+		Value:    "Manning Publications Co0",
+		HttpOnly: true,
+	}
+
+	// w.Header().Set("Set-Cookie", c1.String())
+	// w.Header().Add("Set-Cookie", c2.String())
+	http.SetCookie(w, &c1)
+	http.SetCookie(w, &c2)
+}
+
+func getCookie(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	// h := r.Header["Cookie"]
+	// fmt.Fprintln(w, h)
+
+	c1, err := r.Cookie("first_cookie")
+	if err != nil {
+		fmt.Fprintln(w, "Cannot get the first cookie")
+	}
+	fmt.Fprintln(w, c1)
+
+	cs := r.Cookies()
+	fmt.Fprintln(w, cs)
+}
+
+func setMessage(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	msg := []byte("Hello, World!")
+	c := http.Cookie{
+		Name:  "flash",
+		Value: base64.URLEncoding.EncodeToString(msg),
+	}
+	http.SetCookie(w, &c)
+}
+
+func showMessage(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	c, err := r.Cookie("flash")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			fmt.Fprintln(w, "Flash Message Not Found")
+		}
+	} else {
+
+		rc := http.Cookie{
+			Name:    "flash",
+			MaxAge:  -1,
+			Expires: time.Unix(1, 0),
+		}
+		http.SetCookie(w, &rc)
+
+		flashMessage, _ := base64.URLEncoding.DecodeString(c.Value)
+		fmt.Fprintln(w, string(flashMessage))
+	}
 }
 
 func main() {
@@ -100,6 +186,13 @@ func main() {
 	mux.POST("/write", writeExample)
 	mux.POST("/writeheader", writeHeaderExample)
 	mux.GET("/redirect", headerExample)
+	mux.GET("/json", jsonExample)
+
+	mux.GET("/set_cookie", setCookie)
+	mux.GET("/get_cookie", getCookie)
+
+	mux.GET("/set_message", setMessage)
+	mux.GET("/show_message", showMessage)
 
 	server := &http.Server{
 		Addr:    "0.0.0.0:8080",
