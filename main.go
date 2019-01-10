5a9a38afc15b875623b9deaed49860cd7bc3b702
Ben Sooraj <bensoorajm@gmail.com>
2019-01-10 21:27:51 +0530

    Creating and writing to a CSV file

--- main.go
+++ main.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"encoding/csv"
 	"os"
+	"strconv"
 )
 
+type Post struct {
+	ID      int
+	Content string
+	Author  string
+}
+
 func main() {
-	data := []byte("Hello, world!\n")
-	err := ioutil.WriteFile("data1", data, 0644)
+	// Writing
+	csvFile, err := os.Create("posts.csv")
 	if err != nil {
 		panic(err)
 	}
+	defer csvFile.Close()
 
-	read1, _ := ioutil.ReadFile("data1")
-	fmt.Printf("%s", read1)
+	allPosts := []Post{
+		Post{ID: 1, Content: "Hello World!", Author: "Sau Sheong"},
+		Post{ID: 2, Content: "Bonjour Monde!", Author: "Pierre"},
+		Post{ID: 3, Content: "Hola Mundo!", Author: "Pedro"},
+		Post{ID: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"},
+	}
 
-	file1, _ := os.Create("data2")
-	defer file1.Close()
+	writer := csv.NewWriter(csvFile)
+	for _, post := range allPosts {
+		line := []string{strconv.Itoa(post.ID), post.Content, post.Author}
+		err := writer.Write(line)
+		if err != nil {
+			panic(err)
+		}
+	}
+	writer.Flush()
 
-	b, _ := file1.Write(data)
-	fmt.Printf("Wrote %v bytes.\n", b)
-
-	file2, _ := os.Open("data2")
-	defer file2.Close()
-
-	chunk := make([]byte, len(data))
-	b, _ = file2.Read(chunk)
-
-	fmt.Printf("Read %d bytes from file\n", b)
-	fmt.Println(string(chunk))
 }
